68e49b2b358b8392613178a4f08f5d08e4760531
shoudaos <shoudaos92@gmail.com>
2016-12-01 10:09:59 +0300

    add mock data for chart

--- server.js
+++ server.js
@@ -1,46 +1,92 @@
 var WebSocketServer = require('ws').Server,
-  port = Number(process.env.PORT || 8000),
-  wss = new WebSocketServer({port: port});
+    port = Number(process.env.PORT || 8000),
+    wss = new WebSocketServer({port: port}),
+    i;
 
 wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log(message);
-  });
 
-  setInterval(function () {
-    if (ws.readyState == 1) {
-      ws.send(JSON.stringify({
-        value: getTableData(),
-        type: 'table'
-      }));
-    }
-  }, 1000);
+    i = 1;
 
-  console.log('connection');
+    ws.on('message', function (message) {
+        console.log(message);
+    });
+
+    setInterval(function () {
+        if (ws.readyState == 1) {
+            ws.send(JSON.stringify({
+                value: getTableData(),
+                type: 'table'
+            }));
+        }
+    }, 1000);
+
+    setInterval(function () {
+        if (ws.readyState == 1) {
+            ws.send(JSON.stringify({
+                value: getChartData(),
+                type: 'chart'
+            }));
+        }
+    }, 1000);
+
+    console.log('connection');
 });
 
-var getTableData = function () {
-  var rangeRandom = function (min, max) {
+var rangeRandom = function (min, max) {
     return Math.random() * (max - min) + min;
-  };
-
-  var a = 100 + Math.floor(rangeRandom(-10, 10));
-  var b = 100 + Math.floor(rangeRandom(-10, 10));
-
-  return {
-    "userId": "abc",
-    "error": false,
-    "errorMessage": "",
-    "data": {
-      "id": "15",
-      "name": "Tomas",
-      "info": {
-        "type": "JPEG",
-        "dimensions": "1800x1420"
-      },
-      "y": "2006",
-      "a": a,
-      "b": b,
-    }
-  }
 };
+
+var getTableData = function () {
+
+    var a = 100 + Math.floor(rangeRandom(-10, 10));
+    var b = 100 + Math.floor(rangeRandom(-10, 10));
+
+    return {
+        "userId": "abc",
+        "error": false,
+        "errorMessage": "",
+        "data": {
+            "id": "15",
+            "name": "Tomas",
+            "info": {
+                "type": "JPEG",
+                "dimensions": "1800x1420"
+            },
+            "y": "2006",
+            "a": a,
+            "b": b
+        }
+    }
+};
+
+var getChartData = function () {
+
+    var a = 100 + Math.floor(rangeRandom(-10, 10));
+    var b = 100 + Math.floor(rangeRandom(-10, 10));
+
+    return {
+        "userid": "abc",
+        "resize": true,
+        "error": false,
+        "errorMessage": "",
+        "data": {
+            "y": ++i,
+            "a": a,
+            "b": b
+        }
+        ,
+        "xkey": "y",
+        "ykeys": [
+            "a",
+            "b"
+        ],
+        "labels": [
+            "Series A",
+            "Series B"
+        ],
+        "lineColors": [
+            "#88C4EE",
+            "#ccc"
+        ]
+    }
+}
